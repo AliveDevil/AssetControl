282dbe5ce9d323c2ec701e528f5ca773339be5b6
Jöran Malek <AliveDevil@users.noreply.github.com>
2015-04-09 12:33:59 +0200

    Client is now able to resolve incomming messages.

--- libAssetControl/Network/Client.cs
+++ libAssetControl/Network/Client.cs
@@ -1,25 +1,37 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.IO.Compression;
 using System.Net;
-using System.Net.Security;
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
 using libAssetControl.Network.Messages;
 
 namespace libAssetControl.Network
 {
+	public delegate void MessageReceivedEventHandler(object message);
+
 	public class Client : IDisposable
 	{
-		public bool Connected { get; private set; }
+		private delegate void MessageHandler(object message);
+
+		public event MessageReceivedEventHandler MessageReceived;
+
+		private BinaryFormatter formatter;
+
+		private Dictionary<Type, MessageHandler> messages;
+		private Action reading;
+
+		private DeflateStream readingStream;
+
+		private IAsyncResult readResult;
 
 		private TcpClient tcpClient;
+
 		private NetworkStream tcpStream;
-		private DeflateStream readingStream;
+
 		private DeflateStream writingStream;
-		private Action reading;
-		private IAsyncResult readResult;
-		private BinaryFormatter formatter;
+
+		public bool Connected { get; private set; }
 
 		public Client(IPAddress target, int port)
 		{
@@ -33,48 +45,9 @@ namespace libAssetControl.Network
 			Initialize();
 		}
 
-		private void TcpClientConnected(IAsyncResult result)
-		{
-			tcpClient.EndConnect(result);
-			Initialize();
-		}
-
-		private void Initialize()
-		{
-			Connected = true;
-			tcpStream = tcpClient.GetStream();
-
-			readingStream = new DeflateStream(tcpStream, CompressionMode.Decompress, true);
-			writingStream = new DeflateStream(tcpStream, CompressionMode.Compress, true);
-			reading = Read;
-			readResult = reading.BeginInvoke(ReadEnded, null);
-		}
-
-		private void ReadEnded(IAsyncResult result)
-		{
-			reading.EndInvoke(result);
-		}
-
-		private void Read()
-		{
-			while (tcpClient.Connected)
-			{
-				if (tcpStream.DataAvailable)
-				{
-					object message = formatter.Deserialize(readingStream);
-					Type t = message.GetType();
-				}
-			}
-		}
-
 		public void Disconnect()
 		{
 			Write(DisconnectMessage.Message);
-		}
-
-		public void Write(object message)
-		{
-			formatter.Serialize(writingStream, message);
 		}
 
 		public void Dispose()
@@ -86,6 +59,84 @@ namespace libAssetControl.Network
 			readResult.AsyncWaitHandle.WaitOne();
 			tcpStream.Dispose();
 			tcpClient.Close();
+		}
+
+		public void Write(object message)
+		{
+			formatter.Serialize(writingStream, message);
+		}
+
+		private void HandleDisconnectMessage(object message)
+		{
+			Disconnect();
+		}
+
+		private void HandleHeloMessage(object message)
+		{
+			Write(HeloMessage.Message);
+		}
+
+		private void Initialize()
+		{
+			messages = new Dictionary<Type, MessageHandler>()
+			{
+				{typeof(HeloMessage), HandleHeloMessage},
+				{typeof(DisconnectMessage), HandleDisconnectMessage}
+			};
+
+			Connected = true;
+			tcpStream = tcpClient.GetStream();
+
+			readingStream = new DeflateStream(tcpStream, CompressionMode.Decompress, true);
+			writingStream = new DeflateStream(tcpStream, CompressionMode.Compress, true);
+			reading = Read;
+			readResult = reading.BeginInvoke(ReadEnded, null);
+		}
+
+		private void OnMessageReceived(object message)
+		{
+			if (MessageReceived != null)
+			{
+				MessageReceived(message);
+			}
+		}
+
+		private void Read()
+		{
+			while (tcpClient.Connected)
+			{
+				if (tcpStream.DataAvailable)
+				{
+					object message = formatter.Deserialize(readingStream);
+					Type t = message.GetType();
+					if (!ResolveMessage(t, message))
+					{
+						OnMessageReceived(message);
+					}
+				}
+			}
+		}
+
+		private void ReadEnded(IAsyncResult result)
+		{
+			reading.EndInvoke(result);
+		}
+
+		private bool ResolveMessage(Type t, object message)
+		{
+			MessageHandler handler;
+			if (messages.TryGetValue(t, out handler))
+			{
+				handler(message);
+				return true;
+			}
+			return false;
+		}
+
+		private void TcpClientConnected(IAsyncResult result)
+		{
+			tcpClient.EndConnect(result);
+			Initialize();
 		}
 	}
 }
