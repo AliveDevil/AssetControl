ac0e8bf782f2ba62c7550e31e50e567c0e20627a
FlashTek <rzrolandzimmermann@gmail.com>
2015-04-09 11:11:08 +0200

    Created first basic layout of the MainWindow of the client; Created basic data structures

---
+++ AssetControl.Client/Data/NotifyPropertyChangedBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetControl.Client.Data
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ AssetControl.Client/DataContext/MainWindowDataContext.cs
@@ -0,0 +1,61 @@
+﻿using AssetControl.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetControl.Client.DataContext
+{
+    public class MainWindowDataContext : NotifyPropertyChangedBase
+    {
+        public MainWindowDataContext()
+        {
+            _remoteObjects = new ObservableCollection<RemoteObject>();
+        }
+
+        private readonly ObservableCollection<RemoteObject> _remoteObjects;
+        public ObservableCollection<RemoteObject> RemoteObjects
+        {
+            get
+            {
+                return _remoteObjects;
+            }
+        }
+
+        private RemoteObject _selectedRemoteObject;
+        public RemoteObject SelectedRemoteObject
+        {
+            get
+            {
+                return _selectedRemoteObject;
+            }
+            set
+            {
+                if (value is Asset)
+                {
+                    CurrentAsset = value as Asset;
+                }
+
+                _selectedRemoteObject = value;
+                onPropertyChanged("SelectedRemoteObject");
+            }
+        }
+
+        private Asset _currentAsset;
+        public Asset CurrentAsset
+        {
+            get
+            {
+                return _currentAsset;
+            }
+            set
+            {
+                _currentAsset = value;
+                onPropertyChanged("CurrentAsset");
+            }
+        }
+    }
+}
