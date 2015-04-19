27ac7dcdc6fe93642c8661362a72febfe3bfa345
Jöran Malek <AliveDevil@users.noreply.github.com>
2015-04-19 20:23:59 +0200

    Fixed Namespace of AuthMessage Moved Data/Asset to Data/Change.

--- libAssetControl/Data/Project.cs
+++ libAssetControl/Data/Project.cs
@@ -11,7 +11,7 @@ namespace libAssetControl.Data
 		public ICollection<User> Users { get; set; }
 		public ICollection<Branch> Branches { get; set; }
 		public ICollection<Commit> Commits { get; set; }
-		public ICollection<Asset> Assets { get; set; }
+		public ICollection<Change> Changes { get; set; }
 
 		public Project(AssetStore store, string name)
 			: base(store)
@@ -20,7 +20,7 @@ namespace libAssetControl.Data
 			Users = new Collection<User>();
 			Branches = new Collection<Branch>();
 			Commits = new Collection<Commit>();
-			Assets = new Collection<Asset>();
+			Changes = new Collection<Change>();
 		}
 	}
 }
---
+++ libAssetControl/Network/Messages/AuthClientMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libAssetControl.Network.Messages
+{
+	public struct AuthClientMessage
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
