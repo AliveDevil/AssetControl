f2bd645dbcdf17e4f346d92446f4a5393d977ca1
Jöran Malek <AliveDevil@users.noreply.github.com>
2015-04-29 18:33:55 +0200

    Updated default structure.

--- AssetControl.Daemon/Program.cs
+++ AssetControl.Daemon/Program.cs
@@ -17,8 +17,30 @@ namespace AssetControl.Daemon
 			FileInfo storeFile = new FileInfo(file);
 			AssetStore store = new AssetStore();
 			if (!storeFile.Exists)
+			{
+				User user = new User(store, "TestUser", "TestPasswordShouldBeSHA512OrSHA256.");
+				Project project = new Project(store, "TestProject");
+				Branch branch = new Branch(project, Guid.Empty, "master");
+				Commit commit = new Commit(project, branch, null, Guid.Empty);
+				Commit childCommit = new Commit(project, branch, commit, new Guid("00000000-0000-0000-0000-000000000001"));
+				Commit childChildCommit = new Commit(project, branch, childCommit, new Guid("00000000-0000-0000-0000-000000000002"));
+				Change change = new Change(project, commit);
+
+				project.Changes.Add(change);
+				project.Commits.Add(childChildCommit);
+				project.Commits.Add(childCommit);
+				project.Commits.Add(commit);
+				project.Branches.Add(branch);
+				project.Users.Add(user);
+
+				store.Projects.Add(project);
+				store.Users.Add(user);
+
 				using (Stream fileStream = storeFile.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
 					store.Save(fileStream);
+			}
+			store.Users.Clear();
+			store.Projects.Clear();
 			using (Stream fileStream = storeFile.Open(FileMode.Open, FileAccess.Read, FileShare.Read))
 				store.Load(fileStream);
 			Host host = new Host(c => new DaemonClient(c, store), 13337);
